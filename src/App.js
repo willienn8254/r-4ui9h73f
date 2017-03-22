83d67593979b2c2557e75e24954960c85b6eb674
welinton <willienn@hotmail.com>
2017-03-22 09:43:23 -0500

    Se agrega funcionalidad repetir

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+	constructor(){
+
+	super();
+	this.state={mensaje: ""}
+
+
+}
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.repetir.bind(this)} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.mensaje}</p>
       </div>
     );
   }
+
+repetir(event){
+
+
+  		this.setState({
+
+  			mensaje: event.target.value
+  		})
+
+
+
+}
 }
 
 export default App;
